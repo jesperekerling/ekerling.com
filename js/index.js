5730804f68a56d82a30ab94e5200999d1e02d8b7
Jesper Ekerling <jesper@forme.se>
2023-10-04 02:14:48 +0200

    CSS and JS

--- js/index.js
+++ js/index.js
@@ -1,23 +1,58 @@
+
+// Start of theme by time of day
 // Source: https://codepen.io/lakshmanan-arumugam/pen/zYZvewO
 
-// https://codepen.io/haxzie/pen/xxKNEGM
-
 function setThemePreference() {
-    var d = new Date();
-    /*
-    * The getHours() method returns the hour (from 0 to 23) of the specified date and time.
-    * Early morning = 0 - 6
-    * Morning = 6 - 12
-    * Evening = 12 - 18
-    * Night = 18 - 23
-    */
-    var currentHour = d.getHours();  
-  
-    if(currentHour >= 20 || currentHour <= 0  ) {
-      document.body.setAttribute("data-theme", "dark_theme") 
-    }else {
-      document.body.setAttribute("data-theme", "light_theme") 
-    }
+  var d = new Date();
+  /*
+  * The getHours() method returns the hour (from 0 to 23) of the specified date and time.
+  * Early morning = 0 - 6
+  * Morning = 6 - 12
+  * Evening = 12 - 18
+  * Night = 18 - 23
+  */
+  var currentHour = d.getHours();  
+
+  if(currentHour >= 20 || currentHour <= 7  ) {
+    document.documentElement.setAttribute("class", "dark_theme") 
+  }else {
+    document.documentElement.setAttribute("class", "light_theme") 
   }
+}
   
   window.onload = setThemePreference;
+
+  // End of theme by time of day
+
+
+
+
+  // Start of theme toggle script
+  // Source: https://codepen.io/haxzie/pen/xxKNEGM
+
+   // function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark_theme') {
+        setTheme('light_theme');
+    } else {
+        setTheme('dark_theme');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'light_theme') {
+        setTheme('light_theme');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('dark_theme');
+      document.getElementById('slider').checked = true;
+    }
+})();
+
